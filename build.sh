3923bd3a30ce63676577943ef078fa6c5633b26a
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-12-19 02:36:46 +0100

    fix git

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ rm -f config.status
 
 # Ubuntu 10.04 (gcc 4.4)
 # extracflags="-O3 -march=native -Wall -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
-
+extracflags="$extracflags -march=native"
 # Debian 7.7 / Ubuntu 14.04 (gcc 4.7+)
 extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
 
