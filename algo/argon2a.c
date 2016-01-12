92487182c4ff1ba4c6c9c56a8fb23e9fdd4d757b
Miouyouyou <dev@miouyouyou.fr>
2016-01-12 01:19:40 +0000

    Version focused on Argon2. This WILL break if you use others scrypt algorithms...
    The optimisations were done by 'constant propagation'. Nothing
    really fancy. This might provide you 20% more performance, at best.

--- algo/argon2a.c
+++ algo/argon2a.c
@@ -6,33 +6,53 @@
 #include <stdio.h>
 
 #include "sha3/ar2/src/argon2.h"
+#include "sha3/ar2/src/cores.h"
 #include "sha3/sj/scrypt-jane.h"
+
+#define T_COSTS 2
+#define M_COSTS 16
+#define MASK 8
+#define ZERO 0
 
 void argon2hash(void *output, const void *input)
 {
 	// these uint512 in the c++ source of the client are backed by an array of uint32
 	uint32_t _ALIGN(32) hashA[8], hashB[8], hashC[8];
-	unsigned int t_costs = 2;
-	unsigned int m_costs = 16;
-	uint32_t mask = 8;
-	uint32_t zero = 0;
 
-	scrypt((const unsigned char *)input, 80,
+	my_scrypt((const unsigned char *)input, 80,
 		(const unsigned char *)input, 80,
-		m_costs/2, 0, 0, (unsigned char *)hashA, 32);
+		(unsigned char *)hashA);
 
-	if ((hashA[0] & mask) != zero)
-		hash_argon2d(hashB, 32, hashA, 32,
-			hashA, 32, t_costs, m_costs);
-	else
-		hash_argon2i(hashB, 32, hashA, 32,
-			hashA, 32, t_costs, m_costs);
+       argon_call(hashB, hashA, hashA, (hashA[0] & MASK) == ZERO);
 
-	scrypt((const unsigned char *)hashB, 32,
+	my_scrypt((const unsigned char *)hashB, 32,
 		(const unsigned char *)hashB, 32,
-		m_costs/2, 0, 0, (unsigned char *)hashC, 32);
+		(unsigned char *)hashC);
 
 	memcpy(output, hashC, 32);
+}
+
+void argon_call(void *out, void *in, void *salt, int type) {
+  argon2_context context;
+
+  context.out = (uint8_t *)out;
+  context.pwd = (uint8_t *)in;
+  context.salt = (uint8_t *)salt;
+
+  /*context.outlen = context.pwdlen = context.saltlen = (uint32_t)32;
+  context.secret = NULL;
+  context.secretlen = 0;
+  context.ad = NULL;
+  context.adlen = 0;
+  context.t_cost = T_COSTS;
+  context.m_cost = M_COSTS;
+  context.lanes = 1;
+  context.threads = 1;
+  context.allocate_cbk = NULL;
+  context.free_cbk = NULL;
+  context.flags = ARGON2_DEFAULT_FLAGS;*/
+
+  argon2_core(&context, type);
 }
 
 int scanhash_argon2(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
@@ -41,9 +61,6 @@ int scanhash_argon2(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 	uint32_t _ALIGN(64) endiandata[20];
 	const uint32_t first_nonce = pdata[19];
 	uint32_t nonce = first_nonce;
-
-	if (opt_benchmark)
-		((uint32_t*)ptarget)[7] = 0x0000ff;
 
 	for (int k=0; k < 20; k++)
 		be32enc(&endiandata[k], ((uint32_t*)pdata)[k]);
